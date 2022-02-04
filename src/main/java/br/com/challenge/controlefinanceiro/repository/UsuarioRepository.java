418a2d3ffa77952228346ddd7808e0420135f666
flavws <persona.fmaria@gmail.com>
2022-02-03 22:11:31 -0300

    autenticação de usuário/senha

---
+++ src/main/java/br/com/challenge/controlefinanceiro/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package br.com.challenge.controlefinanceiro.repository;
+
+import br.com.challenge.controlefinanceiro.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+}
